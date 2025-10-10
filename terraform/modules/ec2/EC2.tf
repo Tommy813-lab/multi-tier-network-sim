1a56a43dfba0ae14254e86caec3df491e926a121
Tommy813 <quietopscb@gmail.com>
2025-10-10 13:05:06 -0400

    Add project screenshots for recruiter visibility

--- terraform/modules/ec2/EC2.tf
+++
@@ -1,9 +0,0 @@
-resource "aws_instance" "this" {
-  ami           = "ami-0c55b159cbfafe1f0" # Amazon Linux 2 (us-east-1)
-  instance_type = var.instance_type
-  subnet_id     = var.subnet_id
-
-  tags = {
-    Name = "multi-tier-ec2"
-  }
-}
--- terraform/modules/ec2/variables.tf
+++
@@ -1,7 +0,0 @@
-variable "subnet_id" {
-  type = string
-}
-
-variable "instance_type" {
-  type = string
-}
