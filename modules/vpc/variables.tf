57db0729433594183ab2333ae0751835ac848b85
Charles Bucher <quietopscb@gmail.com>
2025-10-16 02:33:20 -0400

    Update variables.tf

--- modules/vpc/variables.tf
+++ modules/vpc/variables.tf
@@ -1,3 +1,10 @@
 variable "cidr_block" {
-  type = string
+  description = "The CIDR block for the VPC (must be a valid IPv4 CIDR, e.g., 10.0.0.0/16)"
+  type        = string
+  default     = "10.0.0.0/16"  # optional default
+
+  validation {
+    condition     = can(cidrhost(var.cidr_block, 0))
+    error_message = "The cidr_block must be a valid IPv4 CIDR notation."
+  }
 }
