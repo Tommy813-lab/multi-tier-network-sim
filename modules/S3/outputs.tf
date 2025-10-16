d8ca9c9dbd492dc1d94575af35ea5cff0feca33a
Charles Bucher <quietopscb@gmail.com>
2025-10-16 02:30:03 -0400

    Update outputs.tf

--- modules/S3/outputs.tf
+++ modules/S3/outputs.tf
@@ -1,3 +1,9 @@
-output "bucket_name" {
-  value = aws_s3_bucket.this.bucket
+output "s3_bucket_info" {
+  value = {
+    name             = aws_s3_bucket.this.bucket
+    arn              = aws_s3_bucket.this.arn
+    region           = aws_s3_bucket.this.region
+    website_endpoint = aws_s3_bucket.this.website_endpoint
+  }
+  description = "Information about the S3 bucket"
 }
