a2caceab61d4be205ce96d132562a7377134b230
Charles Bucher <quietopscb@gmail.com>
2025-10-16 02:30:43 -0400

    Add description and validation to bucket_name variable

--- modules/S3/variables.tf
+++ modules/S3/variables.tf
@@ -1,3 +1,9 @@
 variable "bucket_name" {
-  type = string
+  description = "The name of the S3 bucket to create or reference"
+  type        = string
+  default     = "my-default-bucket-name"   # optional, remove if you want to force input
+  validation {
+    condition     = length(var.bucket_name) >= 3 && length(var.bucket_name) <= 63
+    error_message = "Bucket name must be between 3 and 63 characters."
+  }
 }
