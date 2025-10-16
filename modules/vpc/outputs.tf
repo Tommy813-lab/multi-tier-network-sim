5f0f1f78970c32ef8a594af0472ea93fbb420a3b
Charles Bucher <quietopscb@gmail.com>
2025-10-16 02:33:56 -0400

    Add descriptions to VPC output variables

--- modules/vpc/outputs.tf
+++ modules/vpc/outputs.tf
@@ -1,11 +1,17 @@
+# VPC ID
 output "vpc_id" {
-  value = aws_vpc.this.id
+  description = "The ID of the VPC"
+  value       = aws_vpc.this.id
 }
 
-output "public_subnet_id" {
-  value = aws_subnet.public.id
+# Public Subnet IDs (supports multiple AZs)
+output "public_subnet_ids" {
+  description = "List of public subnet IDs"
+  value       = [for s in aws_subnet.public : s.id]
 }
 
+# DB Subnet Group ID
 output "db_subnet_group_id" {
-  value = aws_db_subnet_group.this.id
+  description = "The ID of the DB subnet group"
+  value       = aws_db_subnet_group.this.id
 }
